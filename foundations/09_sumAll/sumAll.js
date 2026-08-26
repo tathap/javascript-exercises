e693092a2be7f02feaa610967155b9874fc3601f
tathap <pandey.tatha@gmail.com>
2026-08-26 19:07:14 -0400

    Complete Exercise 09
    Used typeof to ensure that the input parameters were of the correct type
    and used Math.floor() to help check to see if my parameters were integers.
    I then used math to find a formula for the sum of the numbers from a to b
    assuming a < b.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (typeof a != "number" || typeof b != "number") {
+    return "ERROR";
+  }
 
+  if (a <= 0 || b <= 0 || Math.floor(a) != a || Math.floor(b) != b) {
+    return "ERROR";
+  }
+
+  if (a > b) {
+    let tem = a;
+    a = b;
+    b = tem;
+  }
+
+  let ans = (b + a) * (b - a + 1) / 2;
+  return ans; 
 };
 
 // Do not edit below this line
