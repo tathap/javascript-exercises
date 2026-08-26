41dfaff3a640941e25a4531c60f4dda67de59b1f
tathap <pandey.tatha@gmail.com>
2026-08-26 17:27:23 -0400

    Complete Exercise 08
    I utilized the arguments object (an array-like object accessible within
    functions) to handle multiple parameters. Looped through every undesirable
    value to remove them from the array using splice.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+  let arr = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    ind = arr.indexOf(arguments[i]);
+    while (ind != -1) {
+      arr.splice(ind, 1);
+      ind = arr.indexOf(arguments[i]);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
