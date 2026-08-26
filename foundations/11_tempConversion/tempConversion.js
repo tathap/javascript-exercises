f811b47fcdedfc073c5c3b0e27df0c87dbffd5ee
tathap <pandey.tatha@gmail.com>
2026-08-26 19:19:54 -0400

    Complete Exercise 11
    Wrote functions to convert from Celsius to Fahrenheit and vice versa.
    I utilized the toFixed() method to fix my floating point answers to 1
    decimal place but I had to cast it into a number type since it returns
    a string by default.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let ret = (+temp - 32) / (9/5);
+  if (Math.floor(ret) === ret) {
+    return ret;
+  }
+  return +ret.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let ret = (+temp * 9 / 5) + 32;
+  if (Math.floor(ret) === ret) {
+    return ret;
+  }
+  return +ret.toFixed(1);
 };
 
 // Do not edit below this line
