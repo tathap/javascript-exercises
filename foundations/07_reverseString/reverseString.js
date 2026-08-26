aa30fc93f9cdfd551e5a3a64c8e9c68065b9fc5f
tathap <pandey.tatha@gmail.com>
2026-08-26 17:19:07 -0400

    Complete Exercise 07
    I reversed a given input string using a for loop.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+  ret = "";
+  for (let i = input.length - 1; i >= 0; i--) {
+     ret += input[i];
+  }
+  return ret; 
 };
 
 // Do not edit below this line
